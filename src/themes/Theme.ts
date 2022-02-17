13d8647bd8db8ae3c41ae040a8b80deccd6db2d3
yihaiwu17 <49830887+yihaiwu17@users.noreply.github.com>
2022-02-18 09:51:48 +1300

    set up the global style

---
+++ src/themes/Theme.ts
@@ -0,0 +1,31 @@
+const Theme = {
+  colors: {
+    light: {
+      primary: "#eee",
+      secondary: "#ddd",
+    },
+    dark: {
+      primary: "#222",
+      secondary: "#444",
+    },
+    red: "#ff8585",
+    green: "#85ffb3",
+  },
+  padding: {
+    sm: "1rem",
+    md: "2rem",
+    lg: "4rem",
+  },
+  width: {
+    sm: "500px",
+    md: "1000px",
+    lg: "1500px",
+  },
+  borderRadius: {
+    sm: "5px",
+    md: "10px",
+    lg: "20px",
+  },
+};
+
+export default Theme;
---
+++ src/themes/styled.d.ts
@@ -0,0 +1,33 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      light: {
+        primary: string;
+        secondary: string;
+      };
+      dark: {
+        primary: string;
+        secondary: string;
+      };
+      red: string;
+      green: string;
+    };
+    padding: {
+      sm: string;
+      md: string;
+      lg: string;
+    };
+    width: {
+      sm: string;
+      md: string;
+      lg: string;
+    };
+    borderRadius: {
+      sm: string;
+      md: string;
+      lg: string;
+    };
+  }
+}
